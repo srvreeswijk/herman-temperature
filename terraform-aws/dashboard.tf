f1cbf86b9677bd87cb33c03c354f6c0f48ac0416
Sebastiaan Vreeswijk <s.vreeswijk@gmail.com>
2020-06-06 20:57:39 +0200

    small fix

--- terraform-aws/dashboard.tf
+++ terraform-aws/dashboard.tf
@@ -2,5 +2,5 @@
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = "herman-koeling"
 
-  dashboard_body = templatefile("${path.module}/templates/widgets.tmpl", { things = ["kamer1", "kamer2"], AWS_REGION = "${var.AWS_REGION}"})
+  dashboard_body = templatefile("${path.module}/templates/widgets.tmpl", { things = var.thing_ids, AWS_REGION = "${var.AWS_REGION}"})
 }
