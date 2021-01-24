b58c0f0a59caf1d30432cf673547ea2c8bcafe23
Sebastiaan Vreeswijk <s.vreeswijk@gmail.com>
2021-01-24 15:25:11 +0100

    made alarm 3 out of 3 datapoints

--- terraform-aws/dashboard.tf
+++ terraform-aws/dashboard.tf
@@ -11,8 +11,8 @@ resource "aws_cloudwatch_metric_alarm" "alarm" {
 
   alarm_name                = "Temperatuur te hoog voor ${each.key}"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "2"
-  datapoints_to_alarm       = "2"
+  evaluation_periods        = "3"
+  datapoints_to_alarm       = "3"
   metric_name               = each.key
   namespace                 = "herman/temp"
   period                    = "300"
